4f68d04ee4def8b6ca4c6158e0bd53fb09e4ceab
Alexa Chávez <yosoyaleksa@gmail.com>
2018-10-15 12:52:49 -0500

    challenge backend developer

---
+++ src/com/example/linio/linioChallenge.java
@@ -0,0 +1,29 @@
+package com.example.linio;
+
+public class linioChallenge {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+
+            while (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("LINIANOS");
+                break;
+            }
+
+            while (i % 3 == 0 && i % 5 != 0) {
+                System.out.println("LINIO");
+                break;
+            }
+
+            while (i % 5 == 0 && i % 3 != 0) {
+                System.out.println("IT");
+                break;
+            }
+
+            while (i % 3 != 0 && i % 5 != 0) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
